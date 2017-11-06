c602cfe141859a57b6e5087cf3c8e6e9df3c8c78
Alexandra Sommar <alexandra@sommar.se>
2017-11-06 15:55:09 +0100

    Adds hangman

---
+++ components/Man.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+export default class Man extends React.Component {
+
+    render () {
+        let visible = {
+            hill: this.props.hill,
+            firstgallows: this.props.firstgallows,
+            secondgallows: this.props.secondgallows,
+            thirdgallows: this.props.thirdgallows,
+            fourthgallows: this.props.fourthgallows,
+            head: this.props.head,
+            eyeone: this.props.eyeone,
+            eyetwo: this.props.eyetwo,
+            body: this.props.body,
+            leftArm: this.props.leftArm,
+            rightArm: this.props.rightArm,
+            leftLeg: this.props.leftLeg,
+            rightLeg: this.props.rightLeg,
+        }
+
+        return (
+            <View style={styles.wrapper}>
+                <View style={visible.hill}>
+                    <Image
+                        source={require('../assets/img/hill.png')}
+                    />
+                </View>
+                <View style={visible.firstgallows}>
+                    <Image
+                        source={require('../assets/img/firstgallows.png')}
+                    />
+                </View>
+                <View style={visible.secondgallows}>
+                    <Image
+                        source={require('../assets/img/secondgallows.png')}
+                    />
+                </View>
+                <View style={visible.thirdgallows}>
+                    <Image
+                        source={require('../assets/img/thirdgallows.png')}
+                    />
+                </View>
+                <View style={visible.fourthgallows}>
+                    <Image
+                        source={require('../assets/img/fourthgallows.png')}
+                    />
+                </View>
+                <View style={visible.head}>
+                    <Image
+                        source={require('../assets/img/head.png')}
+                    />
+                </View>
+                <View style={visible.eyeone}>
+                    <Image
+                        source={require('../assets/img/eye.png')}
+                    />
+                </View>
+                <View style={visible.eyetwo}>
+                    <Image
+                        source={require('../assets/img/eye.png')}
+                    />
+                </View>
+                <View style={visible.body}>
+                    <Image
+                        source={require('../assets/img/body.png')}
+                    />
+                </View>
+                <View style={visible.leftArm}>
+                    <Image
+                        source={require('../assets/img/leftarm.png')}
+                    />
+                </View>
+                <View style={visible.rightArm}>
+                    <Image
+                        source={require('../assets/img/rightarm.png')}
+                    />
+                </View>
+                <View style={visible.leftLeg}>
+                    <Image
+                        source={require('../assets/img/leftleg.png')}
+                    />
+                </View>
+                <View style={visible.rightLeg}>
+                    <Image
+                        source={require('../assets/img/rightleg.png')}
+                    />
+                </View>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    }
+});
--- components/Play.js
+++ components/Play.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Font } from 'expo';
 import Button from 'react-native-button';
+import Man from '../components/Man';
 
 
 import { StackNavigator } from 'react-navigation';
@@ -14,20 +15,113 @@ export default class PlayScreen extends React.Component {
           words: [],
           pressed: false,
           guess: [],
+          wrong: [],
+          gameOver: false,
+          gameWon: false,
+          hill: {height: 0, width: 0},
+          firstgallows: {height: 0, width: 0},
+          secondgallows: {height: 0, width: 0},
+          thirdgallows: {height: 0, width: 0},
+          fourthgallows: {height: 0, width: 0},
+          head: {height: 0, width: 0},
+          eyeone: {height: 0, width: 0},
+          eyetwo: {height: 0, width: 0},
+          body: {height: 0, width: 0},
+          leftArm: {height: 0, width: 0},
+          rightArm: {height: 0, width: 0},
+          leftLeg: {height: 0, width: 0},
+          rightLeg: {height: 0, width: 0},
         }
-        this.checkWord = this.checkWord.bind(this);
+
     }
+
 
     checkWord (letter, key) {
         this.keys.push(key);
-        this.setState({ pressed: true, guess: this.state.guess.concat([letter])});
+        this.setState({ pressed: true});
+        const currentKey = this.state.words.map((name, key) => {
+            let currWord = name.word.toUpperCase();
+            if (currWord.includes(letter)) {
+                this.setState({guess: this.state.guess.concat([letter])});
+            } else {
+                this.setState({wrong: this.state.wrong.concat([letter])},
+                this.countWrong
+            );
+
+            }
+        })
+
     }
 
-    repeat (str, count) {
-      var array = [];
-      for(var i = 0; i < count;)
-          array[i++] = str;
-      return array.join('');
+    countWrong () {
+        switch (this.state.wrong.length) {
+            case 1:
+                return this.setState({
+                    hill: {height: 100, width: 100, position: 'absolute', bottom: -10, left: 100}
+                });
+            case 2:
+                return this.setState({
+                    firstgallows: {height: 100, width: 100, position: 'absolute', bottom: 130, left: 158}
+                });
+            case 3:
+                return this.setState({
+                    secondgallows: {height: 100, width: 20, position: 'absolute', top: -215, left: 140}
+                });
+            case 4:
+                return this.setState({
+                    thirdgallows: {height: 20, width: 20, position: 'absolute', top: -217, left: 154}
+                });
+            case 5:
+                return this.setState({
+                    fourthgallows: {height: 20, width: 20, position: 'absolute', top: -220, left: 210}
+                });
+            case 6:
+                return this.setState({
+                    head: {height: 20, width: 20, position: 'absolute', top: -196, left: 200}
+                });
+            case 7:
+                return this.setState({
+                    eyeone: {height: 200, width: 200, position: 'absolute', top: -189, left: 204}
+                });
+            case 8:
+                return this.setState({
+                    eyetwo: {height: 200, width: 200, position: 'absolute', top: -189, left: 212}
+                });
+            case 9:
+                return this.setState({
+                    body: {height: 200, width: 200, position: 'absolute', top: -178, left: 200}
+                });
+            case 10:
+                return this.setState({
+                    leftArm: {height: 200, width: 200, position: 'absolute', top: -175, left: 184}
+                });
+            case 11:
+                return this.setState({
+                    rightArm: {height: 200, width: 200, position: 'absolute', top: -175, left: 221}
+                });
+            case 12:
+                return this.setState({
+                    leftLeg: {height: 200, width: 200, position: 'absolute', top: -134, left: 195}
+                });
+            case 13:
+                return this.setState({
+                    rightLeg: {height: 200, width: 200, position: 'absolute', top: -134, left: 216}, gameOver: true
+                });
+
+        }
+    }
+
+    winorLose () {
+        const countWords = this.state.words.map((name, key) => {
+            let curr = name.word.length;
+
+            if (name.word.length == this.state.wrong.length) {
+                console.log('Game over!');
+            }
+            if (name.word.length == this.state.guess.length) {
+                this.setState({gameWon: true});
+            }
+        })
     }
 
     static navigationOptions = {
@@ -47,6 +141,7 @@ export default class PlayScreen extends React.Component {
 
     }
       render() {
+          const { navigate } = this.props.navigation;
           const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                   'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
                   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -54,7 +149,7 @@ export default class PlayScreen extends React.Component {
           const buttonItems = alphabet.map((letter, key) =>
 
             <Button
-              containerStyle={{padding:7, height:35, overflow:'hidden', borderRadius:4, marginTop: 10, backgroundColor: 'white'}}
+              containerStyle={{padding:7, height:35, overflow:'hidden', borderRadius:4, margin: 5, backgroundColor: 'white'}}
               style={this.keys.includes(key) ? styles.disbutton : styles.singleButton}
               key={key}
               onPress={() => this.checkWord(letter, key)}
@@ -64,15 +159,20 @@ export default class PlayScreen extends React.Component {
 
           );
 
-         var displayWord = this.state.words.map((letter, key) => {
+         const displayWord = this.state.words.map((letter, key) => {
              var currentLetter = letter.word.toUpperCase();
 
              var displayLetter = '';
              for (var i = 0; i < currentLetter.length; i++) {
                  displayLetter += this.state.guess.find(x => x == currentLetter[i]) ? currentLetter[i] : '-';
              }
-             return (<Text key={key} style={styles.dashes}>{displayLetter}</Text>)
+             return (<Text key={key} style={styles.dashes}>{displayLetter}</Text>);
          })
+
+         const gameover = (<View style={styles.wrap}><Text style={styles.gameover}>GAME OVER!</Text><Text style={styles.newstart} onPress={() => navigate('Home')}>Play again</Text></View>);
+
+         const gamewon =(<View style={styles.wrap}><Text style={styles.gameover}>YOU WON!</Text><Text style={styles.newstart} onPress={() => navigate('Home')}>Play again</Text></View>)
+
 
         return (
           <View style={styles.container}>
@@ -80,7 +180,33 @@ export default class PlayScreen extends React.Component {
                 {buttonItems}
 
             </View>
-            {displayWord}
+
+            {this.winorLose()}
+
+            {this.state.gameOver ?
+                gameover
+                : this.state.gameWon ?
+                gamewon
+                : displayWord
+            }
+            {this.state.wrong.length==0 ? null : (
+            <Man
+                hill={this.state.hill}
+                firstgallows={this.state.firstgallows}
+                secondgallows={this.state.secondgallows}
+                thirdgallows={this.state.thirdgallows}
+                fourthgallows={this.state.fourthgallows}
+                head={this.state.head}
+                eyeone={this.state.eyeone}
+                eyetwo={this.state.eyetwo}
+                body={this.state.body}
+                leftArm={this.state.leftArm}
+                rightArm={this.state.rightArm}
+                leftLeg={this.state.leftLeg}
+                rightLeg={this.state.rightLeg}
+            />
+        )}
+
 
           </View>
         );
@@ -100,23 +226,57 @@ const styles = StyleSheet.create({
       color: '#fff',
   },
   buttons: {
-      flex: 1,
+      display: 'flex',
       flexDirection: 'row',
       flexWrap: 'wrap',
-      marginTop: 20
+      justifyContent: 'space-around',
+      width: 330,
+      marginLeft: 20,
+      marginTop: 10,
+
   },
   singleButton: {
       color: 'green',
+
   },
   disButton: {
       color: 'white',
+
   },
 
 
   dashes: {
       flex: 1,
-      fontSize: 44,
+      fontSize: 28,
       textAlign: 'center',
       letterSpacing: 10,
+      marginTop: 20,
+  },
+  wrap: {
+      flex: 1,
+      justifyContent: 'center',
+      marginTop: -300,
+      alignItems: 'center',
+  },
+  gameover: {
+      fontSize: 20,
+      textAlign: 'center',
+      letterSpacing: 10,
+  },
+  newstart: {
+      fontSize: 19,
+      textAlign: 'center',
+      backgroundColor: 'white',
+      marginTop: 10,
+      width: 100,
+      padding: 5,
+      borderRadius: 5,
+      borderWidth: 1,
+      borderColor: 'white',
+      overflow: 'hidden'
+
   }
+
+
+
 });
